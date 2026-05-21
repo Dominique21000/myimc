ebf3b88f442176d1bdfb278539b3627c6bf015b0
Dominique SAUVIGNON <dominique.sauvignon@etik.com>
2026-05-21 08:43:37 +0200

    modif of module-info and rename of a run file

---
+++ myimc-launch.sh
@@ -0,0 +1,1 @@
+java --module-path $JAVAFX_HOME/lib --add-modules javafx.controls,javafx.fxml -jar target/myimc-1.0-SNAPSHOT.jar
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
 module fr.dominiquesauvignon.myimc {
-	requires javafx.controls;
-	requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
-	// Ouvre les packages pour la réflexion FXML
-    opens fr.dominiquesauvignon.myimc to javafx.fxml;
+    opens fr.dominiquesauvignon.myimc to javafx.fxml, javafx.graphics;
     opens fr.dominiquesauvignon.myimc.controller to javafx.fxml;
-
-    // Exporte le package principal pour qu'il soit accessible par d'autres modules si nécessaire
-    // (Utiles pour les tests ou une future architecture multi-modules)
-    exports fr.dominiquesauvignon.myimc;
-    
-    // Si vous avez des interfaces ou classes publiques dans le package controller à exporter :
-    // exports fr.dominiquesauvignon.myimc.controller; 
 }
